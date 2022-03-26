efa26d5457c131c54323b48c52ace74c81979af8
Asad ullah <38780235+asadullah097@users.noreply.github.com>
2022-03-26 14:49:47 +0500

    scaler and object type in graphql

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("hello");
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+ 
+  type profile{
+    name: String
+    email:String
+    age:Int
+    weight:Float
+    IsDeveloper:Boolean
+    
+  }
+  type Query {
+    Employee:profile,
+  },
+
+`);
+
+var root = {
+  Employee: () => {
+    return {
+      name: "asad",
+      email: "asad@gmail.com",
+      age: 25,
+      weight: 80.5,
+      isDeveloper: true
+    }
+  }
+};
+const app = express()
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(4000, () => {
+  console.log('app is listing on localhost:4000/graphql');
+})
